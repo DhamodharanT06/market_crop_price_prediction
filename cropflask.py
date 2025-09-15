bed3e859535f441ed69417673f2f61a7f7a436db
DhamodharanT06 <dhamodharant45@gmail.com>
2025-09-15 11:48:03 +0530

    Update cropflask.py

--- cropflask.py
+++ cropflask.py
@@ -40,5 +40,6 @@ def crop_input():
     return render_template('croppage.html', prediction=predicted_value)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
